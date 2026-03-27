00d786755b1f7ca92be045fe632ce0f416c3c377
solomigg0-debug <solomigg0@gmail.com>
2026-03-26 23:30:37 -0300

    commit

--- src/examples/example.lua
+++ src/examples/example.lua
@@ -1,4 +1,4 @@
-local Noctaer = loadstring(game:HttpGet("LINK_AQUI"))()
+local Noctaer = loadstring(game:HttpGet("https://raw.githubusercontent.com/solomigg0-debug/noctaer-ui/refs/heads/main/src/Noctaer.lua"))()
 
 local Window = Noctaer:CreateWindow({
     Title = "Noctaer",
